bf600183ad7db51069a1656361531edb78bf0bda
stableXO2 <oranges.master@outlook.com>
2020-10-20 15:16:21 +0800

    deploy

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,10 +1,8 @@
-const StableXFactory = artifacts.require("StableXFactory");
-const feeToSetter = '0x94AA0EfFb7cb7C612Ddf665EaF26FDBB9B540cd2';
-const WBNB = {
-    mainnet:'0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    testnet:'0xae13d989dac2f0debff460ac112a837c89baa7cd'
-};
+const StableXFactory = artifacts.require('StableXFactory')
+const feeToSetter = '0x94AA0EfFb7cb7C612Ddf665EaF26FDBB9B540cd2'
 
-module.exports = async (deployer) => {
-    await deployer.deploy(StableXFactory, feeToSetter)
-};
+module.exports = async deployer => {
+  await deployer.deploy(StableXFactory, feeToSetter)
+  const factory = await StableXFactory.deployed()
+  console.log(await factory.INIT_CODE_PAIR_HASH())
+}
