be9839669f1506ee410a6f3c2b48021ff0e7bc1c
stableXO2 <oranges.master@outlook.com>
2020-10-20 15:17:09 +0800

    feat: updated

--- migrations/2_deploy_contract.js
+++
@@ -1,8 +0,0 @@
-const StableXFactory = artifacts.require('StableXFactory')
-const feeToSetter = '0x94AA0EfFb7cb7C612Ddf665EaF26FDBB9B540cd2'
-
-module.exports = async deployer => {
-  await deployer.deploy(StableXFactory, feeToSetter)
-  const factory = await StableXFactory.deployed()
-  console.log(await factory.INIT_CODE_PAIR_HASH())
-}
