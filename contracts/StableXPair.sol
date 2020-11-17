6cf8f240ef2aadc79d0b0b95796d386718eb9674
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2020-11-18 00:21:17 +0800

    Update StableXPair.sol

--- contracts/StableXPair.sol
+++ contracts/StableXPair.sol
@@ -85,7 +85,7 @@ contract StableXPair is IStableXPair, StableXERC20 {
         emit Sync(reserve0, reserve1);
     }
 
-    // if fee is on, mint liquidity equivalent to 1/6th of the growth in sqrt(k)
+    // if fee is on, mint liquidity equivalent to 4/5th of the growth in sqrt(k)
     function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) {
         address feeTo = IStableXFactory(factory).feeTo();
         feeOn = feeTo != address(0);
