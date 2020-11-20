ef6cc03e537d69c26fe7c9e8aedf6f9af8be613b
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2020-11-20 11:05:24 +0800

    add curly braces  to avoid stack too deep

--- contracts/StableXPair.sol
+++ contracts/StableXPair.sol
@@ -143,15 +143,14 @@ contract StableXPair is IStableXPair, StableXERC20 {
         bool feeOn = _mintFee(_reserve0, _reserve1);
     
         uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
+        
+        { // scope for amount{0,1}, avoids stack too deep errors
         amount0 = liquidity.mul(balance0) / _totalSupply; // using balances ensures pro-rata distribution
         amount1 = liquidity.mul(balance1) / _totalSupply; // using balances ensures pro-rata distribution
         require(amount0 > 0 && amount1 > 0, 'StableX: INSUFFICIENT_LIQUIDITY_BURNED');
-
-
     // Implement withdrawal fee, where a 1% fee is levied on withdrawals if feeOn, to incentivize people to hold longer in the pools
     // This fee will be future split between STAX stakers and LP providers, to be governed by governance in the future
-    // Holders of STAX can be refunded to users from the feeTo address (STAX community treasury) in a separate program, based on their STAX holdings
-    // Thus, liquidity providers who hold or stake STAX will get the benefit of free or reduced withdrawal fees in rebate program handled from feeTo.
+    // If this fee is determined to be too punitive, this can be manually refunded to users on an ad-hoc basis from the STAX community Treasury
         
         if (feeOn) {
             uint fee = liquidity.div(100);
@@ -170,10 +169,11 @@ contract StableXPair is IStableXPair, StableXERC20 {
             _safeTransfer(_token0, to, amount0);
             _safeTransfer(_token1, to, amount1);
         }
-
+        }
+        { // scope for balance{0,1}, avoids stack too deep errors
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
-     
+        }
         _update(balance0, balance1, _reserve0, _reserve1);
         if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
         emit Burn(msg.sender, amount0, amount1, to);
