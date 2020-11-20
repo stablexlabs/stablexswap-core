b8a99ce1e7f56f27793f0ad8f99f420b553ffee8
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2020-11-20 17:07:59 +0800

    Update StableXPair.sol

--- contracts/StableXPair.sol
+++ contracts/StableXPair.sol
@@ -135,23 +135,28 @@ contract StableXPair is IStableXPair, StableXERC20 {
         (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
         address _token0 = token0;                                // gas savings
         address _token1 = token1;                                // gas savings
-        address feeTo = IStableXFactory(factory).feeTo();
+        
         uint balance0 = IERC20(_token0).balanceOf(address(this));
         uint balance1 = IERC20(_token1).balanceOf(address(this));
         uint liquidity = balanceOf[address(this)];
 
         bool feeOn = _mintFee(_reserve0, _reserve1);
     
-        uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
         
         { // scope for amount{0,1}, avoids stack too deep errors
+        // feeTo is also only used in this scope
+        // _totalSupply is also only used to calculate amount{0,1}.
+        address feeTo = IStableXFactory(factory).feeTo();
+        uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
+        
         amount0 = liquidity.mul(balance0) / _totalSupply; // using balances ensures pro-rata distribution
         amount1 = liquidity.mul(balance1) / _totalSupply; // using balances ensures pro-rata distribution
         require(amount0 > 0 && amount1 > 0, 'StableX: INSUFFICIENT_LIQUIDITY_BURNED');
     // Implement withdrawal fee, where a 1% fee is levied on withdrawals if feeOn, to incentivize people to hold longer in the pools
     // This fee will be future split between STAX stakers and LP providers, to be governed by governance in the future
     // If this fee is determined to be too punitive, this can be manually refunded to users on an ad-hoc basis from the STAX community Treasury
-        
+    // There should be no cases in which feeOn is true and feeTo is not a real address.  
+    // Fee is only used in this scope  
         if (feeOn) {
             uint fee = liquidity.div(100);
 
@@ -173,8 +178,8 @@ contract StableXPair is IStableXPair, StableXERC20 {
         { // scope for balance{0,1}, avoids stack too deep errors
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
-        }
         _update(balance0, balance1, _reserve0, _reserve1);
+        }
         if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
         emit Burn(msg.sender, amount0, amount1, to);
     }
